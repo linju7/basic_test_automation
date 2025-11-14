25b7036463a53c4f8595ce6fad64464137f3e2b6
USER <user@AL02529596-669.local>
2025-11-14 18:32:52 +0900

    구성원 custom field automation done

--- tests/test_users_custom_field.py
+++ tests/test_users_custom_field.py
@@ -1,5 +1,6 @@
 from automation.modules.users_custom_field.create import create_users_custom_field
 from automation.modules.users_custom_field.update import update_users_custom_field
+from automation.modules.users_custom_field.delete import delete_users_custom_field
 from tests.conftest import app_state, logged_in_page
 
 
@@ -11,4 +12,9 @@ def test_01_users_custom_field_create_flow(logged_in_page):
 def test_02_users_custom_field_update_flow(logged_in_page):
     """구성원 커스텀 필드 수정 테스트"""
     assert update_users_custom_field(logged_in_page, app_state=app_state), "구성원 커스텀 필드 수정 실패"
+
+
+def test_03_users_custom_field_delete_flow(logged_in_page):
+    """구성원 커스텀 필드 삭제 테스트"""
+    assert delete_users_custom_field(logged_in_page, app_state=app_state), "구성원 커스텀 필드 삭제 실패"
 
